049a550d40036a8c8bd10df4faa4a818b06a793f
David Suarez <david.m.suarez@gmail.com>
2025-06-22 14:45:09 -0400

    Fixed typing and reset the episode and season index.

--- main.py
+++ main.py
@@ -23,14 +23,17 @@ def pad_left(x: Union[str, int], pad: str, target_length: int):
     return res
 
 class MLP():
+
+
     def __init__(self, generation: str, begin_season: int = 1, 
-                 begin_episode: int = 1, export_path: str = '.') -> None:
+                 begin_episode: int = 1, export_path: str = '.', 
+                 url: Optional[str] = None) -> None:
         self.gen = generation
         self.base_url = 'https://static.heartshine.gay'
         self.s = begin_season
         self.e = begin_episode
         self.url_log = []
-        self.url = None
+        self.url = url
         self.export_path = export_path
 
     def validate_url(self) -> None:
@@ -41,7 +44,7 @@ class MLP():
     def log_url(self):
         self.validate_url()
         with open(f'{self.export_path}/log.txt', 'a') as f:
-            f.write(self.url + '\n') 
+            f.write(self.url + '\n')  # type: ignore
 
     def build_url(self) -> None:
         self.url = f'{self.base_url}/{self.gen}/'
@@ -51,7 +54,7 @@ class MLP():
     def download(self) -> Union[str, requests.Response]:
         self.validate_url()
 
-        r = requests.get(self.url)
+        r = requests.get(self.url) # type: ignore
 
         if r.url != self.url:
             if self.e == 1:
@@ -71,7 +74,7 @@ if __name__ == '__main__':
     target = Target('./target')
     target.build(force = True)
 
-    mlp = MLP(generation = 'g4-fim', begin_season = 14, begin_episode = 23, 
+    mlp = MLP(generation = 'g4-fim', begin_season = 1, begin_episode = 1, 
               export_path = target.target)
 
     while True:
