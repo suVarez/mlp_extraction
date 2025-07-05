58b0c97fb79d55d443f2cc51de53d546e5156f1d
David Suarez <david.m.suarez@gmail.com>
2025-07-04 21:53:30 -0400

    some refactoring with creating subdirectories for seasons and naming the episodes.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from typing import Optional, Union
 from target import Target
 from time import sleep
 from playwright.sync_api import sync_playwright
+from lxml import etree as et
 
 def pad_left(x: Union[str, int], pad: str, target_length: int):
 
@@ -24,6 +25,16 @@ def pad_left(x: Union[str, int], pad: str, target_length: int):
 
     return res
 
+def get_rendered_html(url):
+    with sync_playwright() as p:
+        browser = p.chromium.launch(headless=True)
+        page = browser.new_page()
+        page.goto(url)
+        page.wait_for_load_state('networkidle')  # Wait for network to be idle
+        html = page.content()
+        browser.close()
+        return html
+
 
 
 
@@ -36,6 +47,7 @@ class MLP():
                  url: Optional[str] = None) -> None:
         self.gen = generation
         self.base_url = 'https://static.heartshine.gay'
+        self.details_url = f'https://{generation}.heartshine.gay'
         self.s = begin_season
         self.e = begin_episode
         self.url_log = []
@@ -47,34 +59,11 @@ class MLP():
         if self.url is None:
             raise ValueError('The url cannot be None. Use build_url()')
 
-    def validate_rendered_html(self) -> None:
-        if self.html is None:
-            raise ValueError('The html content cannot be None. Use get_rendered_html()')
-
 
     def log_url(self):
         self.validate_url()
         with open(f'{self.export_path}/log.txt', 'a') as f:
             f.write(self.url + '\n')  # type: ignore
-
-    def get_rendered_html(self, url: str) -> None:
-        se = f's={pad_left(self.s, "0", 2)}&e={pad_left(self.e, "0", 2)}'
-        url = f'https://fim.heartshine.gay/?{se}&res=1080&lo=0'
-        
-        with sync_playwright() as p:
-            browser = p.chromium.launch( headless = True)
-            page = browser.new_page()
-            page.goto(url)
-            page.wait_for_load_state('networkidle')  # Wait for network to be idle
-            html = page.content()
-            browser.close()
-
-        self.html = html
-    
-    def get_season_name(self):
-        self.validate_rendered_html()
-
-        
 
 
     def build_url(self) -> None:
@@ -98,31 +87,86 @@ class MLP():
 
         return r
         
-    
+
+def get_rendered_html(url: str) -> Optional[str]:
+    html = None
+    with sync_playwright() as p:
+        browser = p.chromium.launch( headless = True)
+        page = browser.new_page()
+        page.goto(url)
+        page.wait_for_load_state('networkidle')  # Wait for network to be idle
+        html = page.content()
+        browser.close()
+
+    return html
+
+
 
 if __name__ == '__main__':
 
     target = Target('./target')
     target.build()
 
-    mlp = MLP(generation = 'g4-fim', begin_season = 1, begin_episode = 1, 
+    # loop through all seasons of a generation to get the season and episode 
+    # names
+    parser = et.HTMLParser()
+    season_names = []
+    episode_names = {}
+    max_season = None
+    s = 1
+    generation = 'g4-fim'
+    gen_prefix = 'fim'
+    while max_season is None or s <= max_season:
+
+        url = f'https://{gen_prefix}.heartshine.gay/?s={s}&e=1&res=480&lo=0'
+
+
+        rendered_html = get_rendered_html(url)
+
+        root = et.fromstring(rendered_html, parser)
+
+        if max_season is None:
+            seasons = root.find('.//select[@id="seasList"]')
+            season_names = [child.text for child in seasons.getchildren()]
+            episode_names = {s:[] for s in season_names}
+            max_season = len(season_names)
+
+        episodes = root.find('.//select[@id="epList"]')
+
+        for child in episodes.getchildren():
+
+            episode_names[season_names[s-1]].append(child.text)
+        print(episode_names)
+        s += 1        
+
+
+
+    mlp = MLP(generation = generation, begin_season = 1, begin_episode = 1, 
               export_path = target.target)
 
-    while True:
+
+    s = 1
+    e = 1
+    current_season = season_names[s-1]
+    current_episodes = episode_names[current_season]
+    while s < max_season and e < len(current_episodes):
         sleep(5)
+        if e == 1:
+            target.build_subdirectory(current_season)
 
-        target_file = f'{mlp.s}-{mlp.e}.mp4'
+        target_file = f'{current_season}/{current_episodes[e-1]}.mp4'
 
-
+        mlp.e = e
+        mlp.s = s
         mlp.build_url()
 
         print("Downloading starts...\n")
         r = mlp.download()
         if r == 'continue':
-            mlp.s += 1
-            mlp.e = 1
+            s += 1
+            e = 1
             continue
-        if r == 'break':
+        elif r == 'break':
             break
 
         print("Writing to file...\n")
@@ -132,6 +176,6 @@ if __name__ == '__main__':
                 f.write(chunk)
         print("Download completed..!!")
 
-        mlp.e += 1
+        e += 1
 
 
